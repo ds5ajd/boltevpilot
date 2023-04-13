06a5f835f9bfe9a61e2e2044ace7e87997151a12
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2023-04-13 18:11:07 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -178,10 +178,10 @@ class CarInterface(CarInterfaceBase):
                                                                          tire_stiffness_factor=tire_stiffness_factor)
 
     # longitudinal
-    ret.longitudinalTuning.kpBP = [0., 30.0 * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.20, 1.0, 0.76] #[1.10, 1.0, 0.76]
-    ret.longitudinalTuning.kiBP = [0., 30.0 * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.135, 0.125, 0.115] #[0.145, 0.125, 0.115]
+    ret.longitudinalTuning.kpBP = [0., 18.0 * CV.KPH_TO_MS, 30.0 * CV.KPH_TO_MS, 80.0 * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kpV = [1.20, 1.16, 1.0, 0.76] #[1.10, 1.0, 0.76]
+    ret.longitudinalTuning.kiBP = [0., 18.0 * CV.KPH_TO_MS, 30.0 * CV.KPH_TO_MS, 80.0 * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kiV = [0.135, 0.135, 0.125, 0.115] #[0.145, 0.125, 0.115]
     
     ret.longitudinalTuning.deadzoneBP = [0., 30.*CV.KPH_TO_MS]
     ret.longitudinalTuning.deadzoneV = [0., 0.10]
