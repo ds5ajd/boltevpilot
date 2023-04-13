1ae507692865edfff2ab44b769d041c36595d64e
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2023-04-13 22:04:51 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -35,7 +35,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
     # return params.ACCEL_MIN, params.ACCEL_MAX
     accel_max_bp = [10., 20., 35., 50., 70., 80.]
-    accel_max_v = [0.8, 1.0, 0.8, 0.85, 1.0, 0.95] #[0.8, 1.0, 0.95]
+    accel_max_v = [0.8, 0.95, 0.8, 0.85, 1.0, 0.95] #[0.8, 1.0, 0.95]
 
     return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
 
@@ -179,16 +179,16 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 18.0 * CV.KPH_TO_MS, 30.0 * CV.KPH_TO_MS, 80.0 * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.20, 1.16, 1.0, 0.76] #[1.10, 1.0, 0.76]
+    ret.longitudinalTuning.kpV = [1.18, 1.16, 1.0, 0.76] #[1.10, 1.0, 0.76]
     ret.longitudinalTuning.kiBP = [0., 18.0 * CV.KPH_TO_MS, 30.0 * CV.KPH_TO_MS, 80.0 * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.135, 0.135, 0.125, 0.115] #[0.145, 0.125, 0.115]
+    ret.longitudinalTuning.kiV = [0.135, 0.135, 0.127, 0.115] #[0.145, 0.125, 0.115]
     
     ret.longitudinalTuning.deadzoneBP = [0., 30.*CV.KPH_TO_MS]
     ret.longitudinalTuning.deadzoneV = [0., 0.10]
     ret.longitudinalActuatorDelayLowerBound = 0.13 #0.13
     ret.longitudinalActuatorDelayUpperBound = 0.25 #0.15
     
-    ret.stopAccel = -2.1
+    ret.stopAccel = -2.3
     ret.stoppingDecelRate = 4.0
     ret.vEgoStopping = 0.5
     ret.vEgoStarting = 0.5
