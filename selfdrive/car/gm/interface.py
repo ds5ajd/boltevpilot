b3efd04992295f3bc820bc480fb88c236b238931
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2023-04-26 14:46:00 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -188,7 +188,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayLowerBound = 0.13 #0.12
     ret.longitudinalActuatorDelayUpperBound = 0.15 #0.15
     
-    ret.stopAccel = -1.5
+    ret.stopAccel = -1.2
     ret.stoppingDecelRate = 4.5 #4.0
     ret.vEgoStopping = 0.5
     ret.vEgoStarting = 0.5
