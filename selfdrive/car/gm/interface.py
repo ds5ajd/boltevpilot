a0c4751e9824107e3969f28ddde3c4ed0b06988f
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2023-05-09 16:17:10 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -190,7 +190,7 @@ class CarInterface(CarInterfaceBase):
     
     ret.stopAccel = -2.0
     ret.stoppingDecelRate = 4.0
-    ret.vEgoStopping = 0.6
+    ret.vEgoStopping = 1.0
     ret.vEgoStarting = 0.3
     ret.stoppingControl = True
     
