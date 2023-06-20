9511f4effbaefffe943933f678744e6766b225e5
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2023-06-20 17:58:49 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -100,7 +100,7 @@ class CarController():
       self.comma_pedal = 0.0 # Must be set by zero, or cannot re-acceling when stopped. - jc01rho.
 
     elif CS.adaptive_Cruise:
-      ConstAccel = interp(CS.out.vEgo, [3.0 * CV.KPH_TO_MS, 6.0 * CV.KPH_TO_MS, 9.0 * CV.KPH_TO_MS, 18.0 * CV.KPH_TO_MS, 100.0 * CV.KPH_TO_MS], [0.05, 0.07, 0.11, 0.15, 0.2125]) #0.15, 0.2125
+      ConstAccel = interp(CS.out.vEgo, [1.0 * CV.KPH_TO_MS, 6.0 * CV.KPH_TO_MS, 9.0 * CV.KPH_TO_MS, 18.0 * CV.KPH_TO_MS, 100.0 * CV.KPH_TO_MS], [0.05, 0.07, 0.12, 0.15, 0.2125]) #0.15, 0.2125
       accelFomula = ((actuators.accel - ConstAccel) / 8.0)
       accelFomula = round(accelFomula+0.00001, 4)
       
@@ -138,7 +138,7 @@ class CarController():
             self.stoppingStateTimeWindowsActiveCounter += 1
             actuators.stoppingStateTimeWindowsActiveCounter = self.stoppingStateTimeWindowsActiveCounter
             if self.stoppingStateTimeWindowsActiveCounter > 0 :
-              actuators.pedalStartingAdder = interp(CS.out.vEgo, [0.0, 5.0 * CV.KPH_TO_MS ,12.5 * CV.KPH_TO_MS , 25.0 * CV.KPH_TO_MS, 35.0 * CV.KPH_TO_MS], [0.0050, 0.0800, 0.1250, 0.1550, 0.0250])
+              actuators.pedalStartingAdder = interp(CS.out.vEgo, [0.0, 1.0 * CV.KPH_TO_MS, 6.0 * CV.KPH_TO_MS, 12.5 * CV.KPH_TO_MS , 25.0 * CV.KPH_TO_MS, 35.0 * CV.KPH_TO_MS], [0.0, 0.0050, 0.0500, 0.1250, 0.1550, 0.0250])
               if d > 0:
                 actuators.pedalDistanceAdder = interp(d, [1,6,8, 9.5, 15, 30], [-1.0250 ,-0.5000 ,-0.0525 ,  -0.0100 ,0.0175,0.1000])
               actuators.pedalAdderFinal = (actuators.pedalStartingAdder + actuators.pedalDistanceAdder)
@@ -179,7 +179,7 @@ class CarController():
       #if actuators.accel < -0.15 :
         can_sends.append(gmcan.create_regen_paddle_command(self.packer_pt, CanBus.POWERTRAIN))
         actuators.regenPaddle = True #for icon
-      elif controls.LoC.pid.f < - 0.85 :
+      elif controls.LoC.pid.f < - 0.90 :
         can_sends.append(gmcan.create_regen_paddle_command(self.packer_pt, CanBus.POWERTRAIN))
         actuators.regenPaddle = True #for icon
         minMultipiler = interp(CS.out.vEgo, [20 * CV.KPH_TO_MS ,  30 * CV.KPH_TO_MS , 60 * CV.KPH_TO_MS ,120 * CV.KPH_TO_MS ], [0.850, 0.750, 0.625, 0.150])
