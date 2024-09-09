b4eb2d0867a38d4da818aeb18bf5bef4240e0f75
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2024-09-09 15:15:27 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -145,7 +145,7 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 1.0
       # still working on improving lateral
       ret.steerRateCost = 0.35 # default 0.5
-      ret.steerActuatorDelay = 0.1 # default 0.
+      ret.steerActuatorDelay = 0.05 # default 0.
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.14, 0.24], [0.01, 0.021]]
       ret.lateralTuning.pid.kdBP = [0.]
